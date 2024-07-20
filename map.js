f2d1f66dfc90e0ea92beb20f41e14a5528286465
Really_true_dude <andrekor2004@gmail.com>
2024-07-21 00:07:12 +0300

    Regions on map

--- map.js
+++ map.js
@@ -1,4 +1,15 @@
-function showRegInfo(region) {
-    regId = region.id;
-    console.log(regId);
+function showRegInfo(event, region) {
+    let regName = region.getAttribute("name");
+    let x = event.clientX + 15;
+    let y = event.clientY + 15;
+    let box = document.getElementsByClassName("curRegion")[0];
+    box.style.left = x + "px";
+    box.style.top = y + "px";
+    box.style.display = "block";
+    box.innerHTML = regName;
+}
+
+function removeRegInfo() {
+    let box =  document.getElementsByClassName("curRegion")[0];
+    box.style.display = "none";
 }
